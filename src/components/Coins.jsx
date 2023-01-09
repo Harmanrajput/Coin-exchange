cc3360ce8ecee51fde1206914da58e8a71b6aeb3
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-09 20:51:35 +0530

    there are alot of functionality tht is reusable and lot of component build in this

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import axios from "axios";
 import { server } from "../index";
 import { useState } from 'react';
-import {Button, Container,Heading,HStack,Image,Text, VStack} from '@chakra-ui/react';
+import {Button, Radio, Container,HStack,RadioGroup} from '@chakra-ui/react';
 import Loader from './Loader';
 import ErrorComponent from './ErrorComponent';
 import CoinCard from './CoinCard';
@@ -42,7 +42,7 @@ const btns=new Array(132).fill(1);
   }, [currency,page]);
 
   if (error)
-  return <ErrorComponent message={"Error While Fetching coins"} />;
+  return <ErrorComponent message={"Error While Fetching Coins"} />;
 
 return (
   <Container maxW={"container.xl"}>
@@ -50,6 +50,13 @@ return (
       <Loader />
     ) : (
       <>
+      <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+        <HStack spacing={"4"}>
+          <Radio value={"inr"}>₹ INR</Radio>
+          <Radio value={"usd"}>$ DOLLARS</Radio>
+          <Radio value={"eur"}>€ EURO</Radio>
+        </HStack>
+      </RadioGroup>
         <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
           {coins.map((i) => (
             <CoinCard
@@ -66,7 +73,7 @@ return (
         <HStack w={"full"} overflow={"auto"} p={"8"}>
         {
         btns.map((item,index)=>(
-            <Button bgColor={"blackAlpha.900"} color={"white"} onClick={()=>changePage(index+1)}>{index+1}</Button>
+            <Button  key={index} bgColor={"blackAlpha.900"} color={"white"} onClick={()=>changePage(index+1)}>{index+1}</Button>
         ))}
         </HStack>
       </>
