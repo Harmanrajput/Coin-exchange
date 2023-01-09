5904b9f414bb67d5f09e5dde11d8601067198af0
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-09 20:50:11 +0530

    error page

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,8 +1,17 @@
+import { Alert, AlertIcon } from '@chakra-ui/react'
 import React from 'react'
 
-const ErrorComponent = ({message}) => {
+const ErrorComponent = ({ message }) => {
   return (
-    <div>{message}</div>
+    <Alert status='error'
+      position={"fixed"}
+      bottom={"4"}
+      left={"50%"}
+      transform={"translatex(-50%)"}
+      w={"container.lg"}>
+      <AlertIcon />
+      {message}
+    </Alert>
   )
 }
 
