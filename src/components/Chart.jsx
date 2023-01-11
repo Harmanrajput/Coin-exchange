805951e83d4b16298e73c4dede7046857ba6b651
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-11 20:06:29 +0530

    the new component in making

---
+++ src/components/Chart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2';
+import {Chart as chartjs} from "chart.js";
+const Chart = () => {
+  return (
+    <div>Chart</div>
+  )
+}
+
+export default Chart
