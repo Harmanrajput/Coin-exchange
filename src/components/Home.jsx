068fa206d4d6ee08b176b140f04f844b28d06bcb
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-12 15:03:55 +0530

    the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import btcSrc from "../Assets/btc.png"
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box bgColor={"blackAlpha.800"} w={"full"} h={"85vh"}>
+     
+     <Image
+          w={"full"}  h={"full"} objectFit={"contain"} src={btcSrc} filter={"grayscale(1)"}/>
+ <Text fontSize={"6xl"} textAlign={"left"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={"-20"} > Crypto Moniter
+      </Text>
+      <Text fontSize={"6xl"} textAlign={"right"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={"-20"} > Check out
+      </Text>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
