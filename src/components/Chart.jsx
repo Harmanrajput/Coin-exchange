35cd69cd92ee2d691ec8b67361314528dcece38a
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-12 15:03:17 +0530

    check out the chart compoent with the for loop

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -21,24 +21,36 @@ ChartJS.register(
   Legend
 );
 
-const Chart = ({arr=[],currency,day) => {
-const prices =[1,2,3];
-const date=["12/3/23" ,"23/3/55" , "23/4/44"];
-const data ={};
+const Chart = ({ arr = [], currency, days }) => {
+  const prices = [];
+  const date = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (days === "24h") date.push(new Date(arr[i][0]).toLocaleTimeString());
+    else date.push(new Date(arr[i][0]).toLocaleDateString());
+    prices.push(arr[i][1]);
+  }
+
+  const data = {
+    labels: date,
+    datasets: [
+      {
+        label: `Price in ${currency}`,
+        data: prices,
+        borderColor: "rgb(255,99,132)",
+        backgroundColor: "rgba(255,99,132,0.5)",
+      },
+    ],
+  };
+
   return (
-   <Line options={{
-    responsive:true,
-   }}
-   
-   data={{
-    labels:date,
-    datasets:[{
-        label:`Price in ${currency}`,
-        data:prices,borderColor:"rgb(255,99,132)",
-        backgroundColor:"rgb(255,99,132)",
-    }]
-   }}/>
+    <Line
+      options={{
+        responsive: true,
+      }}
+      data={data}
+    />
   );
 };
 
-export default Chart
+export default Chart;
