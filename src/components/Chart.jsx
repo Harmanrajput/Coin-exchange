88dd6930230c55dd38c1ead3ef9e02583fbe8420
Harman Rajput <harman.rajput9805@gmail.com>
2023-01-12 13:00:47 +0530

    the chart component

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import { Line } from 'react-chartjs-2';
-import {Chart as chartjs} from "chart.js";
-const Chart = () => {
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Chart = ({arr=[],currency,day) => {
+const prices =[1,2,3];
+const date=["12/3/23" ,"23/3/55" , "23/4/44"];
+const data ={};
   return (
-    <div>Chart</div>
-  )
-}
+   <Line options={{
+    responsive:true,
+   }}
+   
+   data={{
+    labels:date,
+    datasets:[{
+        label:`Price in ${currency}`,
+        data:prices,borderColor:"rgb(255,99,132)",
+        backgroundColor:"rgb(255,99,132)",
+    }]
+   }}/>
+  );
+};
 
 export default Chart
